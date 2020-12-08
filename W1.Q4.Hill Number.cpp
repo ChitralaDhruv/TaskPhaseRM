2a3e852660fd87ef53f6be209b88703ef0afcf06
drew-dc <74125195+drew-dc@users.noreply.github.com>
2020-12-08 11:00:11 -0800

    Worksheet 1 Question 4
    My program is working absolutely fine with 5 or more digited numbers. But it is giving outputs for less than 5 digited numbers. I could not figure out the problem.

---
+++ W1.Q4.Hill Number.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Num;
+    cout<<"Enter Number  ";
+    cin>>Num;
+    int digits=0 ;
+    int ArrayElement;
+    int Var=Num;
+    int x=0 , count=1;
+    for(;Var!=0;digits++)
+    Var=Var/10;
+    
+    int numArray[digits];
+    
+    for(int n=0;Num!=0;n++)
+    {
+      ArrayElement=Num%10;
+      numArray[n]=ArrayElement;
+      Num=Num/10;
+       
+    }  
+    for(;numArray[x]<numArray[x+1] && x<digits;x++)
+    {
+        count=count+1;
+    } 
+    for(;numArray[x]>numArray[x+1] && x<digits;x++)
+    {
+        count=count+1;
+    }
+    if(count==digits)
+      cout<<"It is a Hill Number";
+    else
+      cout<<"It is not a Hill Number";
+    
+    return 0;
+    
+    
+    
+}
