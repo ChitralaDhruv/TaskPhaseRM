25cb8fde29ae57a261bb3a15dcd73912b0305cd1
drew-dc <74125195+drew-dc@users.noreply.github.com>
2020-12-09 10:48:10 +0530

    Updated the Finished Code.
    Found a bug and corrected the code.

--- W1.Q4.Hill Number.cpp
+++ W1.Q4.Hill Number.cpp
@@ -14,6 +14,13 @@ int main()
     for(;Var!=0;digits++)
     Var=Var/10;
     
+    if(digits<3)
+    {  
+      cout<<"It is not a Hill Number";
+    }  
+    else
+    {
+    
     int numArray[digits];
     
     for(int n=0;Num!=0;n++)
@@ -23,19 +30,23 @@ int main()
       Num=Num/10;
        
     }  
+    
     for(;numArray[x]<numArray[x+1] && x<digits;x++)
     {
         count=count+1;
     } 
-    for(;numArray[x]>numArray[x+1] && x<digits;x++)
+
+    for(;numArray[x]>numArray[x+1] && x<digits-1;x++)
     {
         count=count+1;
     }
+
     if(count==digits)
       cout<<"It is a Hill Number";
     else
       cout<<"It is not a Hill Number";
     
+    }
     return 0;
     
     
